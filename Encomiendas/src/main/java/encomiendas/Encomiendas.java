b353fe3e025e62000d3af6a38958485a216c8d10
Richard Rivera <rich7mcs@hotmail.com>
2024-07-03 12:43:42 -0500

    initial commit - created project

---
+++ Encomiendas/src/main/java/encomiendas/Encomiendas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package encomiendas;
+
+/**
+ *
+ * @author Richard
+ */
+public class Encomiendas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Encomiendas/src/main/java/encomiendas/bd.sql
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Richard
+ * Created: 3 jul 2024
+ */
+
+create database encomiendas;
+use encomiendas;
