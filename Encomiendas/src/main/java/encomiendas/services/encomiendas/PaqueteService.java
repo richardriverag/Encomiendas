62d63554341f11f2380c2049bc42a8055a5c6c25
Kevin Sebastian D <kedose@outlook.it>
2024-07-28 21:23:58 -0500

    feat: añadir nuevas vistas para la gestión de usuarios
    - Se añadieron nuevas vistas para la gestión de usuarios:
      - AEdiciónUsuario.java
      - PanelAEditarUsuarios.java
      - PanelAIngresoUsuarios.java
      - PanelAVisualizarUsuarios.java
      - PanelPerfilClientes.java
    - Estas vistas incluyen las estructuras básicas para la visualización.
    - Pendiente la implementación de los métodos correspondientes.

--- Encomiendas/src/main/java/encomiendas/services/encomiendas/PaqueteService.java
+++ Encomiendas/src/main/java/encomiendas/services/encomiendas/PaqueteService.java
@@ -1,43 +1,43 @@
-package encomiendas.services.encomiendas;
-
-import encomiendas.model.data.encomiendas.PaqueteRepository;
-import encomiendas.model.entity.encomiendas.Paquete;
-
-
-import java.sql.SQLException;
-import java.util.List;
-
-public class PaqueteService {
-
-    private final PaqueteRepository paqueteRepository;
-
-    public PaqueteService(PaqueteRepository paqueteRepository) {
-        this.paqueteRepository = paqueteRepository;
-    }
-
-    //Obtener toda los paquetes
-    public List<Paquete> getAllPaquetes() throws SQLException {
-        return paqueteRepository.findAll();
-    }
-
-    //Obtener un paquete por id
-    public Paquete getPaqueteById(Integer id) throws SQLException {
-        return paqueteRepository.getById(id);
-    }
-
-    //Guardar un paquete
-    public void savePaquete(Paquete paquete) throws SQLException {
-        paqueteRepository.save(paquete);
-    }
-
-    //Eliminar un paquete
-    public void deletePaquete(Integer id) throws SQLException {
-        paqueteRepository.delete(id);
-    }
-
-    //calcular el precio del del paquete
-    public double calcularPrecioTotal(Integer idEncomienda) throws SQLException {
-        Paquete paquete = getPaqueteById(idEncomienda);
-        return paquete.calcularPrecio();
-    }
-}
+//package encomiendas.services.encomiendas;
+//
+//import encomiendas.model.data.encomiendas.PaqueteRepository;
+//import encomiendas.model.entity.encomiendas.Paquete;
+//
+//
+//import java.sql.SQLException;
+//import java.util.List;
+//
+//public class PaqueteService {
+//
+//    private final PaqueteRepository paqueteRepository;
+//
+//    public PaqueteService(PaqueteRepository paqueteRepository) {
+//        this.paqueteRepository = paqueteRepository;
+//    }
+//
+//    //Obtener toda los paquetes
+//    public List<Paquete> getAllPaquetes() throws SQLException {
+//        return paqueteRepository.findAll();
+//    }
+//
+//    //Obtener un paquete por id
+//    public Paquete getPaqueteById(Integer id) throws SQLException {
+//        return paqueteRepository.getById(id);
+//    }
+//
+//    //Guardar un paquete
+//    public void savePaquete(Paquete paquete) throws SQLException {
+//        paqueteRepository.save(paquete);
+//    }
+//
+//    //Eliminar un paquete
+//    public void deletePaquete(Integer id) throws SQLException {
+//        paqueteRepository.delete(id);
+//    }
+//
+//    //calcular el precio del del paquete
+//    public double calcularPrecioTotal(Integer idEncomienda) throws SQLException {
+//        Paquete paquete = getPaqueteById(idEncomienda);
+//        return paquete.calcularPrecio();
+//    }
+//}
