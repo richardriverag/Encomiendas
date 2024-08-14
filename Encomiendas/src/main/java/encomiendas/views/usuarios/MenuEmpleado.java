074eb58ae6e4ce163470d7bf8e1218961d43abf2
JotaValla <jimmy.valladares@epn.edu.ec>
2024-08-13 21:45:42 -0500

    feat: Se arreglo el merge xd

--- Encomiendas/src/main/java/encomiendas/views/usuarios/MenuEmpleado.java
+++ Encomiendas/src/main/java/encomiendas/views/usuarios/MenuEmpleado.java
@@ -1,7 +1,6 @@
 package encomiendas.views.usuarios;
 
 import encomiendas.views.encomiendas.JFEncomiendas;
-import encomiendas.views.transporte.JFModTransporte;
 import javax.swing.JFrame;
 
 /**
@@ -72,8 +71,6 @@ public class MenuEmpleado extends javax.swing.JFrame {
         jPMétodoEditar4 = new javax.swing.JPanel();
         jPAEEcomiendas = new javax.swing.JPanel();
         btnAbrirEncomiendas = new javax.swing.JButton();
-        btnEncomiendas = new javax.swing.JButton();
-        JTPEmpleado1 = new javax.swing.JTabbedPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Menú - Empleado");
@@ -104,11 +101,6 @@ public class MenuEmpleado extends javax.swing.JFrame {
 
         JTPEmpleado.setBackground(new java.awt.Color(200, 0, 0));
         JTPEmpleado.setForeground(new java.awt.Color(255, 255, 255));
-        JTPEmpleado.addChangeListener(new javax.swing.event.ChangeListener() {
-            public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                JTPEmpleadoStateChanged(evt);
-            }
-        });
 
         jPMétodoEditar5.setBackground(new java.awt.Color(204, 0, 51));
         jPMétodoEditar5.setMinimumSize(new java.awt.Dimension(690, 430));
@@ -463,10 +455,6 @@ public class MenuEmpleado extends javax.swing.JFrame {
 
         JTPEmpleado.addTab("Encomiendas", jPanel4);
 
-        JTPEmpleado1.setBackground(new java.awt.Color(200, 0, 0));
-        JTPEmpleado1.setForeground(new java.awt.Color(255, 255, 255));
-        JTPEmpleado.addTab("Transporte", JTPEmpleado1);
-
         jPMenuEmpleado.add(JTPEmpleado, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 0, -1, 440));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -541,7 +529,6 @@ public class MenuEmpleado extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JTabbedPane JTPEmpleado;
-    public javax.swing.JTabbedPane JTPEmpleado1;
     public javax.swing.JButton btEClientes;
     public javax.swing.JButton btEConductores;
     public javax.swing.JButton btECredencial;
